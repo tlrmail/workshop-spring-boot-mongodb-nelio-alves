5d5f98ca6f33be2a09c217e8f9199a7dae9783c3
thiago.leite <thiago.leite@dataprev.gov.br>
2023-06-27 19:11:41 -0300

    Endpoint para retornar os posts de um usuário

--- demo/src/main/java/com/nelioalves/workshopmongodb/resources/UserResources.java
+++ demo/src/main/java/com/nelioalves/workshopmongodb/resources/UserResources.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nelioalves.workshopmongodb.domain.Post;
 import com.nelioalves.workshopmongodb.domain.User;
 import com.nelioalves.workshopmongodb.dto.UserDTO;
 import com.nelioalves.workshopmongodb.services.UserService;
@@ -59,4 +60,11 @@ public class UserResources {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(value = "/{id}/posts",method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+	}
+
 }
