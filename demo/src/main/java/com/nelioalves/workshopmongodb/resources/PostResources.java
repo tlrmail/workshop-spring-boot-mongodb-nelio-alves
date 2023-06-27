606e60782faec4a32e7a65657e5f305aac4dbeaa
thiago.leite <thiago.leite@dataprev.gov.br>
2023-06-27 19:36:51 -0300

    Obtendo um post por id

---
+++ demo/src/main/java/com/nelioalves/workshopmongodb/resources/PostResources.java
@@ -0,0 +1,34 @@
+package com.nelioalves.workshopmongodb.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.workshopmongodb.domain.Post;
+import com.nelioalves.workshopmongodb.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResources {
+
+	@Autowired
+	private PostService service;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll() {
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+
+}
---
+++ demo/src/main/java/com/nelioalves/workshopmongodb/services/PostService.java
@@ -0,0 +1,33 @@
+package com.nelioalves.workshopmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.workshopmongodb.domain.Post;
+import com.nelioalves.workshopmongodb.domain.User;
+import com.nelioalves.workshopmongodb.repository.PostRepository;
+import com.nelioalves.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public List<Post> findAll(){
+		return repository.findAll(); 
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> optional = repository.findOne(Example.of(new Post(id,null,null,null,null)));
+		Post post = optional.get();
+		if(post == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		return post;
+	}
+}
