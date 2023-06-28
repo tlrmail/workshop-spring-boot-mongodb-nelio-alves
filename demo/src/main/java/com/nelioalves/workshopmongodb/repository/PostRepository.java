337472efa30aaf35fbdbb08599fcdc3adbeb5fe5
thiago.leite <thiago.leite@dataprev.gov.br>
2023-06-28 09:21:38 -0300

    Consulta simples com @Query

--- demo/src/main/java/com/nelioalves/workshopmongodb/repository/PostRepository.java
+++ demo/src/main/java/com/nelioalves/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.nelioalves.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.nelioalves.workshopmongodb.domain.Post;
@@ -10,6 +11,9 @@ import com.nelioalves.workshopmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") // ?0 -> o 0 indica a posição zero, o 'i' indica ignorecase.
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
